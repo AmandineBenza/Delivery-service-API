0228dce8dc9f77ed3ccc6d0527d5acc8ed14e0cf
Amandine Benza <amandine.benza@etu.unice.fr>
2018-10-07 19:00:48 +0200

    no message

--- run.sh
+++ run.sh
@@ -1,14 +1,22 @@
-echo 'Joe want to eat some food'
-serverIp=192.168.99.100:8080
-jsonFoodResponse= curl "http://$serverIp/DSA/FOOD"
-
-(echo $jsonFoodResponse) >> ok.txt
+echo 'Jean want to eat some food'
+serverIp=localhost:8080
+curl -X POST "http://$serverIp/DSA/UPDATE"
+jsonFoodResponse= curl "http://$serverIp/DSA/CATALOG"
 
 joesResponse='{"foods" : [{"id" : 1,"restaurantId" : 0,"price" : 13.9,"name" : "Hamburger by Sun City Cafe","description" : "Compose your own burger"}],
 "menus" : []
 }' 
 
+choice='Hamburger%20by%20Sun%20City%20Cafe'
+echo 'Jean chose some food in the first restaurant'
+echo 'He check for the delivery time givin his address in the requests'
+curl -X GET "http://$serverIp/DSA/FOOD/Dessert%20by%20Pizza%20FAFA?address=11avtruc"
 
-echo $joesResponse
-curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "$joesResponse" "http://$serverIp/DSA/FOOD/CHINESE?address=10%20Avenue%20de%20la%20porte%20des%20etoiles%20Nice%2006600&clientid=10000" #TODO remove client id
+echo 'He is now ordering his food'
+curl -i -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "$joesResponse" "http://$serverIp/DSA/FOOD/Jean%20Hubert?address=x"
 
+echo 'The restaurant consult his order'
+curl "$serverIp/DSA/RESTAURANT/Sun%20City%20Cafe/ORDERS"
+echo 'Order is now ready to be sent '
+curl -X POST "$serverIp/DSA/RESTAURANT/ORDERS/9"
+curl POST -X "$serverIp/DSA/COURSIER/ORDERS/Pierre%20Rousseau/10"
